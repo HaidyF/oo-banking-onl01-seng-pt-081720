45b7c49c844ac6a92b565b15f06cae5c3ca28520
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-23 22:59:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
   end
 
   def execute_transaction
+    binding.pry
     if valid? == true && @status == "pending" && @sender.balance >= @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
