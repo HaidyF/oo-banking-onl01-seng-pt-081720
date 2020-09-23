d8dfa8ec27dd6c1562106c9fc5507a9e2945477f
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-23 22:56:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else @sender.balance<@amount
+    else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
